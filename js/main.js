48c4999671e523dc8bc5f3c83f5e4697eb833c8a
j-kowal <janokowal@outlook.com>
2017-10-27 22:15:17 +0100

    map added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//dublin cordinates
+var dublin = {lat: 53.3499612, lng: -6.2637037};
+//map object
+var map; var resizedFlag=true;
 //when document loaded...
 $(document).ready(function () {
     //function getting all the data-src value and disposing logos dinamically.
@@ -5,7 +9,19 @@ $(document).ready(function () {
         let src = $(this).data('src'); 
         $(this).css('background-image', 'url("'+ src +'")');
     });
+    initMap();
 });
+// function init map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: dublin
+    });
+    var marker = new google.maps.Marker({
+      position: dublin,
+      map: map
+    });
+}  
 
 //listener of focus and blur(out of focus) event, applying styling for border arround the search field.
 $('.searchField').focus(function (e) { 
@@ -24,4 +40,15 @@ $('.beer').click(function (e) {
 //when click on pub listener
 $('.pub').click(function (e) { 
     window.location = "/pub";   
+});
+//this function fixing issue of map on bootstrap modal by triggering map resize whenever modal is loaded
+$('#myModal').on('shown.bs.modal', function () {
+    google.maps.event.trigger(map, "resize");
+    //first resize flag is true only when map is loading first time to correct centre position.
+    if(resizedFlag)
+    {
+        map.setCenter(dublin);
+        resizedFlag = false;
+    }
+    
 });
