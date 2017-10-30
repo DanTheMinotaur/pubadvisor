a236878c2901ccf4e03aebccfc34abadd8613833
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-10-30 21:05:42 +0000

    Started drink

--- application/controllers/Drink.php
+++ application/controllers/Drink.php
@@ -3,22 +3,20 @@
  * Created by PhpStorm.
  * User: Daniel
  * Date: 30/10/2017
- * Time: 19:14
+ * Time: 20:47
  */
+
 // Include CodeIgniter REST Controller
 require(APPPATH.'/libraries/REST_Controller.php');
 
-class Api extends REST_Controller {
+class Drink extends REST_Controller {
     function __construct() {
         parent::__construct();
-        // No Point in loading each model when API is called
-        //$this->load->model('drink_model');
-        //$this->load->model('venue_model');
     }
 
     // Get all drinks API call
     // FYI CodeIgnitor recognises requests based on the end of the method, eg. _get, _post
-    function allDrinks_get() {
+    function index_get() {
         // check out is it better to load the models in constructor or the methods???
         $this->load->model('drink_model');
         // Gets drink model defined in models folder which then gets results from DB
@@ -32,16 +30,16 @@ class Api extends REST_Controller {
         }
     }
 
-    // Get all venues API call, works the same as allDrinks_get
-    function allVenues_get() {
-        $this->load->model('venue_model');
+    function drinkByName_get() {
 
-        $all_venues = $this->venue_model->getallvenues();
+        $drink_name = $this->get('drink_name');
 
-        if($all_venues) {
-            $this->response($all_venues, 200);
-        } else {
-            $this->response("No Results Found", 404);
+        if(!$drink_name) {
+            $this->response('No Drink Specified, why would you do that?', 400);
         }
+        // CONTINUE HERE -- Had to stop because think DB needs more info such as
+        // URL Slug for drinks, probably implement this in admin panel so drink name automatically gets slugged
+        // Image location
+        // Need to figure out how angualr handles the images.
     }
 }
