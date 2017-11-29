104d3210f3de170517700209ca0ccac53d9274f4
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-11-29 21:23:27 +0000

    Removed Old Login

---
+++ application/views/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 29/11/2017
+ * Time: 21:19
+ */
+    include 'includes/header.php';
+?>
+<h1>Registration</h1>
+
+<?php
+    include 'includes/footer.php';
+?>
