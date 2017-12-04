5adeda0ffa28107f9b63d8c6b5171d855fe6a4dc
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-04 21:45:06 +0000

    Updated ADMIN user table

--- _database/AdminUsersDatabase.sql
+++ _database/AdminUsersDatabase.sql
@@ -1,8 +1,8 @@
 --CREATE DATABASE PAAdminUsers;
 
-DROP TABLE paadminusers.users;
+DROP TABLE users;
 
-CREATE TABLE paadminusers.users (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   username VARCHAR(100) NOT NULL UNIQUE,
